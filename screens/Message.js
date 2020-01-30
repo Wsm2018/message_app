5b3fd66242c676a74714c3d4a514242d0b98ef49
Wsm2018 <40735297+Wsm2018@users.noreply.github.com>
2020-01-30 11:17:16 +0300

    added more features

--- screens/Message.js
+++ screens/Message.js
@@ -9,13 +9,16 @@ export default ({ message, handleEdit }) => {
   const [user, setUser] = useState(null);
   //console.log(message.from);
 
-  const handleUser = async () => {
-    const user = await db
+  const handleUser = () => {
+    const user = db
       .collection("users")
       .doc(message.from)
-      .get();
-    console.log("user from messages", user.data());
-    setUser(user.data());
+      .onSnapshot(docSnapShot => {
+        console.log("message comp handleUser snapshot", docSnapShot.data());
+        setUser(docSnapShot.data());
+      });
+    // console.log("user from messages", user.data());
+    // setUser(user.data());
   };
 
   useEffect(() => {
@@ -31,14 +34,16 @@ export default ({ message, handleEdit }) => {
 
   return (
     user && (
-      <View style={(styles.getStartedText, { flexDirection: "row", margin: 10 })}>
+      <View
+        style={(styles.getStartedText, { flexDirection: "row", margin: 10 })}
+      >
         <View>
           <Image
             source={{ url: user.photoURL }}
             style={{ width: 100, height: 100 }}
           />
         </View>
-        <View style={{alignItems: "center", margin: 10}}>
+        <View style={{ alignItems: "center", margin: 10 }}>
           <Text>From: {user.displayName}</Text>
           <Text>To: {message.to}</Text>
           <Text>Message: {message.text}</Text>
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, TextInput, Button, Image } from "react-native";
 // import { ExpoConfigView } from "@expo/samples";
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/storage";
 import db from "../db";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
@@ -11,6 +12,7 @@ export default function SettingsScreen() {
   const [displayName, setDisplayName] = useState("");
   const [photoURL, setPhotoURL] = useState("");
   const [permissions, setPermission] = useState(false);
+  const [uri, setURI] = useState("");
 
   const permission = async () => {
     const permission = await ImagePicker.requestCameraRollPermissionsAsync();
@@ -39,12 +41,45 @@ export default function SettingsScreen() {
     //use firebase sotrage
     //upload selected image to default bucket naming with uid
     //get url and set photos
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      // this.setState({ image: result.uri });
+      setPhotoURL(result.uri);
+
+      setURI(result.uri);
+
+      //console.log("not cancelled ", url);
+    }
   };
 
-  const handleSave = () => {
-    // await firebase
-    //   .auth()
-    //   .currentUser.updateProfile({ displayName: displayName, photoURL: photoURL });
+  const handleSave = async () => {
+    if (uri !== "") {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+
+      const putResult = await firebase
+        .storage()
+        .ref()
+        .child(firebase.auth().currentUser.uid)
+        .put(blob);
+
+      const url = await firebase
+        .storage()
+        .ref()
+        .child(firebase.auth().currentUser.uid)
+        .getDownloadURL();
+
+      setPhotoURL(url);
+    }
 
     db.collection("users")
       .doc(firebase.auth().currentUser.uid)
@@ -52,6 +87,7 @@ export default function SettingsScreen() {
         displayName,
         photoURL
       });
+
     handleSet();
   };
 
